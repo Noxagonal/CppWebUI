967426664a29978c9a4bcb4181d489f5ba6656b4
Noxagonal <Noxagonal@gmail.com>
2026-06-30 18:38:04 -0400

    Code cleanup.

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,8 +1,4 @@
-
-#include <iostream>
-
 import UI;
-
 
 auto main() -> int
 {
@@ -12,13 +8,14 @@ auto main() -> int
 
 	app.RegisterPage("/", [&]( ui::PageBuilder& ui ){
 		auto& label1 = ui.Label( "Test" );
- 
+
 		ui.Container( [&]{
 			ui.Label( "Test2" );
 		} );
 		ui.Container( [&]{
 			ui.Button( "Click me!", [&]{
 				counter++;
+
 				// label1.text is a custom property type with getters and
 				// setters so setting it will update the browser immediately.
 				label1.text = "Clicked times: " + std::to_string( counter );
